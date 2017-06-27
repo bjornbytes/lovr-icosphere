a6da8b85d49523c7c694b1a7e85e8d583b0c626b
bjorn <tie.372@gmail.com>
2017-06-26 18:21:25 -0700

    Update to new Mesh API;

--- main.lua
+++ main.lua
@@ -1,13 +1,13 @@
 local icosphere = require 'icosphere'
-local buffer
+local mesh
 
 function lovr.load()
   local vertices, indices = icosphere(2)
-  buffer = lovr.graphics.newBuffer(vertices, 'triangles')
-  buffer:setVertexMap(indices)
+  mesh = lovr.graphics.newMesh(vertices, 'triangles')
+  mesh:setVertexMap(indices)
 end
 
 function lovr.draw()
   lovr.graphics.setWireframe(true)
-  buffer:draw(0, 0, -1, .3, lovr.timer.getTime() * .3)
+  mesh:draw(0, 0, -1, .3, lovr.timer.getTime() * .3)
 end
